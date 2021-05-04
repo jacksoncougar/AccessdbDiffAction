0fc42049a264317f0bac6b70da4c5dc1c343bff5
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-04 16:40:47 -0600

    sdfshere

--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -2,6 +2,6 @@
 echo "command.sh"
 HASH=$1
 RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 {} --no-prompt)
-
-if [ -n "$RESULT" ]; then echo "::error file=app.js,line=10,col=15::DIFF"; fi
+echo "::error file=app.js,line=10,col=15::DIFF"
+echo "::error file=app.js,line=10,col=15::$RESULT"
 exit 0
