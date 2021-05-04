d322cb8d1a83e6241856a0284cecd4a306dfaff0
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-04 17:05:18 -0600

    fsdfs

--- .github/workflows/accessdiff.sh
+++ .github/workflows/accessdiff.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-echo "accessdiff.sh"
 BEFORE="${2%.accdb}.xml"
 AFTER="${1%.accdb}.xml"
 ACCESSDIFF="cat"
 $ACCESSDIFF $1 > "$BEFORE"
 $ACCESSDIFF $2 > "$AFTER"
-ls -al
 git diff --no-ext-diff --no-index  "$BEFORE" "$AFTER"
