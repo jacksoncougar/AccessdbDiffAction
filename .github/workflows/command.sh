998f8de77fa64753a3be845ca103ce15ec3b5d02
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-04 16:59:49 -0600

    fdsfsdf

--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -4,5 +4,5 @@ HASH=$1
 RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH)
 echo "::error file=app.js,line=10,col=15::DIFF"
 echo "$RESULT"
-echo $(git diff-tree --no-commit-id --name-only -r $HASH~1)
+echo $(git diff-tree --no-commit-id --name-only -r $HASH~)
 exit 0
