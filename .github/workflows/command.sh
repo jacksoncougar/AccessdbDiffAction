be65000604a4729f245abff790d530db6f11feed
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-04 17:16:50 -0600

    fdfdfds

--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "command.sh"
 HASH=$1
-RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 {} --no-prompt | sed 's/\n/%0A/g'  )
+RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 {} --no-prompt | sed ':a;N;$!ba;s/\n/%0A/g'  )
 
 if [ -n "$RESULT" ]; then echo "::error file=data/test.accdb,line=1,col=1::$RESULT"; fi
 exit 0
