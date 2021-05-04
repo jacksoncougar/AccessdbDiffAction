3b74fbcfe496aa1e7d2f54578a17dfba37ed5c3f
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-04 16:30:29 -0600

    fsd

--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -3,5 +3,5 @@
 HASH=$1
 RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 {} --no-prompt)
 
-if [ -n "$RESULT" ]; then echo "::error file=app.js,line=10,col=15::$RESULT"; fi
+if [ -n "$RESULT" ]; then echo "::error file=app.js,line=10,col=15::DIFF"; fi
 exit 0
