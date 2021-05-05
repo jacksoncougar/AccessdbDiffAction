1423c4d6156bf4b8ab5403ae2c021d0e343084eb
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-04 18:42:06 -0600

    fix: use proper command

--- .github/workflows/accessdiff.sh
+++ .github/workflows/accessdiff.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 BEFORE="${2%.accdb}.xml"
 AFTER="${1%.accdb}.xml"
-ACCESSDIFF="cat"
+ACCESSDIFF="bin/SolutionCliTools"
 $ACCESSDIFF $1 > "$BEFORE"
 $ACCESSDIFF $2 > "$AFTER"
 git diff --no-ext-diff --no-index  "$BEFORE" "$AFTER"
--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -5,5 +5,5 @@ echo $(git diff-tree --no-commit-id --name-only -r $HASH )
 echo $HASH
 RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 {} --no-prompt | sed ':a;N;$!ba;s/\n/%0A/g'  )
 
-if [ -n "$RESULT" ]; then echo "::error file=data/test.accdb,line=1,col=1::$RESULT"; fi
+if [ -n "$RESULT" ]; then echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT"; fi
 exit 0 
