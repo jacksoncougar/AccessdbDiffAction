8fee2f8a1a52684ba5bd5ae774dc6827c486b3ce
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-10 13:43:17 -0600

    fsd

--- .github/workflows/accessdiff.sh
+++ .github/workflows/accessdiff.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-$BEFORE="${2%.accdb}.xml"
-$AFTER="${1%.accdb}.xml"
-$ACCESSDIFF="$GITHUB_WORKSPACE/bin/SolutionCliTools"
+echo "accessdiff.sh"
+BEFORE="${2%.accdb}.xml"
+AFTER="${1%.accdb}.xml"
+ACCESSDIFF="$GITHUB_WORKSPACE/bin/SolutionCliTools"
 
 $ACCESSDIFF $1 > "$BEFORE"
 $ACCESSDIFF $2 > "$AFTER"
