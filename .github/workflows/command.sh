deae17294a45d02f623344b14a76e39937b5831d
Jackson Cougar Wiebe <jacksoncougar@gmail.com>
2021-05-10 09:26:33 -0600

    Update command.sh

--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "command.sh2"
-# HASH=$1
-# echo $(git diff-tree --no-commit-id --name-only -r $HASH )
-# echo $HASH
-# RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 {} --no-prompt | sed ':a;N;$!ba;s/\n/%0A/g'  )
+HASH=$1
+echo $(git diff-tree --no-commit-id --name-only -r $HASH )
+echo $HASH
+RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 {} --no-prompt | sed ':a;N;$!ba;s/\n/%0A/g'  )
 
-# if [ -n "$RESULT" ]; then echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT"; fi
-# exit 0 
+if [ -n "$RESULT" ]; then echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT"; fi
+exit 0 
