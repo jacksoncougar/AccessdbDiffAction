4b53740b17bb6cc68eada3afdb6515b0894a7f74
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-10 12:49:21 -0600

    fsd

---
+++ .github/workflows/accessdiff.ps1
@@ -0,0 +1,9 @@
+# #!/bin/sh
+# BEFORE="${2%.accdb}.xml"
+# AFTER="${1%.accdb}.xml"
+# ACCESSDIFF="$GITHUB_WORKSPACE/bin/SolutionCliTools"
+# $ACCESSDIFF $1 > "$BEFORE"
+# $ACCESSDIFF $2 > "$AFTER"
+# git diff --no-ext-diff --no-index  "$BEFORE" "$AFTER"
+
+echo "Hi"
--- .github/workflows/command.ps1
+++ .github/workflows/command.ps1
@@ -1,10 +1,14 @@
 $ErrorView = "NormalView"
 echo "command.sh2"
-$HASH=$1
+$HASH=$args[0]
 echo $(git diff-tree --no-commit-id --name-only -r $HASH )
 echo $HASH
 # RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 {} --no-prompt | sed ':a;N;$!ba;s/\n/%0A/g'  )
-$RESULT=@(git diff-tree --no-commit-id --name-only -r $HASH | Select-String accdb) | %{git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 $_ --no-prompt}
-echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT";
+$RESULT=@(git diff-tree --no-commit-id --name-only -r $HASH | Select-String accdb) | %{@($_, $(git difftool --extcmd=.github/workflows/accessdiff.ps1 $HASH~1 $_ --no-prompt))}
+if ($RESULT -eq $null) {
+  echo "Nothing changed"
+} else {
+  echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT[0]";
+}
 
 exit 0 
