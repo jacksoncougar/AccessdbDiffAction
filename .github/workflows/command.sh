5b581de080fba956723dc1dfa25a1cf332348f44
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-10 16:53:32 -0600

    d

--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -3,7 +3,7 @@ echo "command.sh2"
 HASH=$1
 echo $(git diff-tree --no-commit-id --name-only -r $HASH )
 echo $HASH
-RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh HEAD^^ {} --no-prompt | sed ':a;N;$!ba;s/\n/%0A/g'  )
+RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh HEAD^^ {} --no-prompt | sed ':a;N;$!ba;s/\n/%0A/g' )
 # RESULT@(git diff-tree --no-commit-id 
 # --name-only -r cc16265449856e28755041116826a70127cf6ef1 | Select-String accdb) | %{git difftool --extcmd=.github/workflows/accessdiff.sh HEAD~1 $_ --no-prompt}
 
