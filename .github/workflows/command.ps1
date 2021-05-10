0450e2685e4934f8eb9f5afc4c90ca67f50b01b3
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-10 13:51:49 -0600

    fdf

--- .github/workflows/command.ps1
+++ .github/workflows/command.ps1
@@ -1,6 +1,8 @@
 $ErrorView = "NormalView"
 Write-Output "command.ps1"
 $HASH=$args[0]
+Write-Output $(git diff-tree --no-commit-id --name-only -r $HASH)
+Write-Output $(git diff-tree --no-commit-id --name-only -r $HASH | where{$_ -like "*.accdb"})
 $RESULT=@(git diff-tree --no-commit-id --name-only -r $HASH) | where{$_ -like "*.accdb"} | %{git difftool --no-prompt --extcmd=.github/workflows/accessdiff.sh $HASH~1 $_ }
 
 Write-Output $RESULT 
