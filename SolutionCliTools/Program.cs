392e6badca8c6cf226aa5e29b042f343cd175ed6
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-10 14:12:35 -0600

    fd

--- SolutionCliTools/Program.cs
+++ SolutionCliTools/Program.cs
@@ -121,7 +121,7 @@ namespace SolutionCliTools
 			}
 			else
 			{
-				accessConnection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;DRIVER=ODBC Driver 17 for SQL Server;Data Source=" + accessFilePath + ";User Id=Admin;Password=");
+				accessConnection = new OleDbConnection(@"Provider=SQLOLEDB;DRIVER=ODBC Driver 17 for SQL Server;Data Source=" + accessFilePath + ";User Id=Admin;Password=");
 			}
 
 			return accessConnection;
