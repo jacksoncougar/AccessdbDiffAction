f26b3b6d94be7671675126c32717e658849ae8d4
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-10 12:27:05 -0600

    convert to powershell

--- .github/workflows/command.ps1
+++ .github/workflows/command.ps1
@@ -1,11 +1,10 @@
-#!/bin/sh
+$ErrorView = "NormalView"
 echo "command.sh2"
-HASH=$1
+$HASH=$1
 echo $(git diff-tree --no-commit-id --name-only -r $HASH )
 echo $HASH
 # RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 {} --no-prompt | sed ':a;N;$!ba;s/\n/%0A/g'  )
-RESULT=@(git diff-tree --no-commit-id 
---name-only -r cc16265449856e28755041116826a70127cf6ef1 | Select-String accdb) | %{git difftool --extcmd=.github/workflows/accessdiff.sh HEAD~1 $_ --no-prompt}
+$RESULT=@(git diff-tree --no-commit-id --name-only -r cc16265449856e28755041116826a70127cf6ef1 | Select-String accdb) | %{git difftool --extcmd=.github/workflows/accessdiff.sh HEAD~1 $_ --no-prompt}
 echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT";
 
 exit 0 
