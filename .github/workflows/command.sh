ae19eb8a04ba993c6d0c5f5237d47ba71ae9bb87
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-10 17:28:45 -0600

    sd

--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -6,4 +6,4 @@ RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs 
 # RESULT@(git diff-tree --no-commit-id 
 # --name-only -r cc16265449856e28755041116826a70127cf6ef1 | Select-String accdb) | %{git difftool --extcmd=.github/workflows/accessdiff.sh HEAD~1 $_ --no-prompt}
 
-if [ -n "$RESULT" ]; then echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT"; fi
+echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT"
