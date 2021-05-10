29109ea33552bb9f6d5dfd9c3dd4b35dd9dd917b
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-10 13:41:24 -0600

    dfd

--- .github/workflows/command.ps1
+++ .github/workflows/command.ps1
@@ -1,14 +1,12 @@
 $ErrorView = "NormalView"
-echo "command.sh2"
 $HASH=$args[0]
-echo $(git diff-tree --no-commit-id --name-only -r $HASH )
-echo $HASH
-# RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh $HASH~1 {} --no-prompt | sed ':a;N;$!ba;s/\n/%0A/g'  )
-$RESULT=@(git diff-tree --no-commit-id --name-only -r $HASH | Select-String accdb) | %{@($_, $(git difftool --extcmd=.github/workflows/accessdiff.ps1 $HASH~1 $_ --no-prompt))}
-if ($RESULT -eq $null) {
-  echo "Nothing changed"
-} else {
-  echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT[0]";
-}
+$RESULT=@(git diff-tree --no-commit-id --name-only -r $HASH) | where{$_ -like "*.accdb"} | %{git difftool --no-prompt --extcmd=.github/workflows/accessdiff.sh $HASH~1 $_ }
+
+Write-Output $RESULT 
+# if ($RESULT -eq $null) {
+#   echo "Nothing changed"
+# } else {
+#   echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT[0]";
+# }
 
 exit 0 
