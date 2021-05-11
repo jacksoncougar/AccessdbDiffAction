fa873c58b397961e0bdc284fa93d669300005032
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-11 13:59:04 -0600

    test

--- .github/workflows/accessdiff.sh
+++ .github/workflows/accessdiff.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-echo "accessdiff.sh"
-
 BEFORE="${2%.accdb}.xml"
 AFTER="${1%.accdb}.xml"
 ACCESSDIFF=".github/workflows/test.sh"
 
 $ACCESSDIFF $2 "$BEFORE"
 $ACCESSDIFF $1 "$AFTER"
-
-echo "accessdiff.sh"
 
 git diff --no-ext-diff --no-index  $BEFORE $AFTER
--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-echo "command.sh2"
 HASH=$1
-
-RESULT=$(git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh HEAD~1 {} --no-prompt | sed ':a;N;$!ba;s/\n/%0A/g' )
-# RESULT@(git diff-tree --no-commit-id 
-# --name-only -r cc16265449856e28755041116826a70127cf6ef1 | Select-String accdb) | %{git difftool --extcmd=.github/workflows/accessdiff.sh HEAD~1 $_ --no-prompt}
-
-echo "::error file=data/SchemaSy.accdb,line=1,col=1::$RESULT"
+git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh HEAD~1 {} --no-prompt | jq -R -s '.'
