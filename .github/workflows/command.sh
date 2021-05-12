7410aaedb3995dc0f4b17326df0bccbfc32634aa
Jack Wiebe <jwiebe@AVANTISOFT.COM>
2021-05-12 07:23:25 -0600

    rest

--- .github/workflows/command.sh
+++ .github/workflows/command.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 HASH=$1
-git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh HEAD~1 {} --no-prompt | jq -R -s '.'
+echo "\`\`\`diff"
+git diff-tree --no-commit-id --name-only -r $HASH | grep accdb | xargs -I{} git difftool --extcmd=.github/workflows/accessdiff.sh HEAD~1 {} --no-prompt 
+echo "\`\`\`"
