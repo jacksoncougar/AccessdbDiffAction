a0b0253975c6f4d48c005f7c75a2b4ab2a131794
Jack Wiebe <jacksoncougar@gmail.com>
2021-07-19 16:02:43 -0600

    fix: remove extra output

--- .github/workflows/diff-added-accdb-file.sh
+++ .github/workflows/diff-added-accdb-file.sh
@@ -7,7 +7,4 @@ ACCESS_TO_JSON=".github/workflows/export-accdb-tables-to-json.sh"
 echo "" >"$BEFORE"
 $ACCESS_TO_JSON $2 "$AFTER"
 
-cat $BEFORE
-cat $AFTER
-
 git diff --no-ext-diff --no-index $BEFORE $AFTER
